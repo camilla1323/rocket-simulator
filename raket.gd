e8a6741fdd6fa3fe1eaba90f858510e19948fca8
camilla <milla23mn@gmail.com>
2025-01-27 12:29:47 +0100

    Fået raket til at flyve ved brug af taster og lavet alien scene

--- raket.gd
+++ raket.gd
@@ -1,11 +1,45 @@
 extends Area2D
-@export var speed = 200
+@export var speed = 150
+var screen_size
+signal hit
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"): #Får raketten til at flyve :O
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size) #Ændre hvor langt på skærmen raketten er flyve, kig på MeCaHaAnNo spil for at ændre
+	
+	
+
+func _on_body_entered(body: Node2D) -> void:
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
