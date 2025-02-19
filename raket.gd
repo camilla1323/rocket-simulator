35f25cc4e512e09f5e60bfa19074cb9abc0e2513
Anne-Lina <annelinakn@gmail.com>
2025-02-19 10:08:22 +0100

    Dødskærm

--- raket.gd
+++ raket.gd
@@ -1,9 +1,7 @@
 extends Area2D
-@export var speed = 80
+@export var speed = 150
 var screen_size
 signal hit
-var velocity: Vector2 = Vector2.ZERO
-
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,7 +11,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	velocity = Vector2.ZERO # The player's movement vector.
+	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"): #Får raketten til at flyve :O
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -35,38 +33,13 @@ func _process(delta: float) -> void:
 	
 
 func _on_body_entered(body: Node2D) -> void:
-	var hmm = body.name
-	if body.name == "månen":  # Tjekker om objektet har navnet "Goal"
-		win()
-	else:
-		die()
-	
-	#hide() # Player disappears after being hit.
-	#hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	#$CollisionShape2D.set_deferred("disabled", true)
-	#print("Info button was pressed!")  # Debugging message
-	#get_tree().change_scene_to_file("res://dødskærm.tscn")
-	
-func die():
 	hide() # Player disappears after being hit.
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred("disabled", true)
-	print("Info button was pressed!")  # Debugging message
-	get_tree().change_scene_to_file("res://dødskærm.tscn")
-	
-func win():
-	print("Info button was pressed!")  # Debugging message
-	get_tree().change_scene_to_file("res://vindeskærm.tscn")
-	
 	
 	
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
-	
-	
-func _physics_process(delta: float) -> void:
-	velocity = velocity
